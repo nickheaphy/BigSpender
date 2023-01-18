4a4d8fb0d6d584722230bf9dc101509d3d70113a
Nick Heaphy <nick@nicnak.net>
2023-01-18 17:33:27 +1300

    Added ingnore account (ugly hack)

--- tools/Reporting.py
+++ tools/Reporting.py
@@ -3,6 +3,8 @@
 import tools.DB as DB
 import tools.date_tools as date_tools
 
+ignore_account1 = 'acc_clccx4vex000308l3fttlelqx'
+
 class Reporting():
 
     # ------------------------------------------------
@@ -10,14 +12,14 @@ class Reporting():
         self.db = DB.DB(filename)
 
     # ------------------------------------------------
-    def spending_by_tag(self, tag, date_from, date_to):
+    def spending_by_tag(self, date_from, date_to, tag):
         
         # tag is a string of tags
         tags = tag.split(' ')
         where = []
         
-        for serachlike in tags:
-            where.append(f"trans_class.tags LIKE '%{serachlike}%'")
+        for searchlike in tags:
+            where.append(f"trans_class.tags LIKE '%{searchlike}%'")
 
         sql = f'''
             SELECT 
@@ -36,8 +38,111 @@ class Reporting():
                 raw_trans.date >= '{date_tools.convert_date_to_beginning_of_day_UTC_isoformat(date_from)}'
             AND
                 raw_trans.date <= '{date_tools.convert_date_to_end_of_day_UTC_isoformat(date_to)}'
+            AND
+                raw_trans.account_id != '{ignore_account1}'
+            ORDER BY
+                trans_class.cat1, trans_class.cat2, trans_class.cat3
         '''
-        print(sql)
+        # print(sql)
 
         transactions = self.db.dbconn.execute(sql)
         return transactions.fetchall()
+
+    # ------------------------------------------------
+    def sum_amount(self, rows):
+        amount = 0
+        for row in rows:
+            amount += row['amount']
+        return amount
+
+    # ------------------------------------------------
+    def spending_by_cat1(self, date_from, date_to, ignore_cat = "", include_cat = ""):
+        
+        ignore_cat = ignore_cat.split(' ')
+        ignore_where = []
+        if ignore_cat[0] != '':
+            for whereitem in ignore_cat:
+                ignore_where.append(f"cat1 <> '{whereitem}'")
+
+        include_cat = include_cat.split(' ')
+        include_where = []
+        if include_cat[0] != '':
+            for whereitem in include_cat:
+                include_where.append(f"cat1 == '{whereitem}'")
+
+        sql = f'''
+        SELECT cat1, SUM(trans_class.amount) as amount
+        FROM trans_class
+        LEFT JOIN raw_trans ON trans_class.transaction_id = raw_trans.transaction_id
+        WHERE
+            raw_trans.date >= '{date_tools.convert_date_to_beginning_of_day_UTC_isoformat(date_from)}'
+        AND
+            raw_trans.date <= '{date_tools.convert_date_to_end_of_day_UTC_isoformat(date_to)}'
+        AND
+                raw_trans.account_id != '{ignore_account1}'
+        '''
+        
+        if len(ignore_where) > 0:
+            sql += " AND " + " AND ".join(ignore_where)
+
+        if len(include_where) > 0:
+            sql += " AND " + " AND ".join(include_where)
+            
+        sql += " GROUP BY cat1 ORDER BY cat1"
+        #print(sql)
+        transactions = self.db.dbconn.execute(sql)
+        return transactions.fetchall()
+
+    # ------------------------------------------------
+    def spending_by_cat2(self, date_from, date_to, ignore_cat = ""):
+        
+        ignore_cat = ignore_cat.split(' ')
+        where = []
+        for whereitem in ignore_cat:
+            where.append(f"cat1 <> '{whereitem}'")
+
+        sql = f'''SELECT cat1, cat2, SUM(trans_class.amount) as amount
+        FROM trans_class
+        LEFT JOIN raw_trans ON trans_class.transaction_id = raw_trans.transaction_id
+        WHERE
+            raw_trans.date >= '{date_tools.convert_date_to_beginning_of_day_UTC_isoformat(date_from)}'
+        AND
+            raw_trans.date <= '{date_tools.convert_date_to_end_of_day_UTC_isoformat(date_to)}'
+        AND
+                raw_trans.account_id != '{ignore_account1}'
+        '''
+
+        if len(where) > 0:
+            sql += " AND " + " AND ".join(where)
+        sql += " GROUP BY cat1, cat2 ORDER BY cat1, cat2"
+        
+        transactions = self.db.dbconn.execute(sql)
+        return transactions.fetchall()
+
+    # ------------------------------------------------
+    def income(self, date_from, date_to):
+        # why did I do it like this?
+        # allcat = self.spending_by_cat1(date_from, date_to)
+
+        # exclude = []
+        # for row in allcat:
+        #     print(row['cat1'])
+        #     if row['cat1'] != 'income' and row['cat1'] != 'transfer':
+        #         exclude.append(row['cat1'])
+        
+        # print(exclude)
+
+        return self.spending_by_cat1(date_from, date_to, include_cat='income')
+
+    # ------------------------------------------------
+    def spend(self, date_from, date_to):
+        # why did I do it like this?
+        # allcat = self.spending_by_cat1(date_from, date_to)
+
+        exclude = 'income transfer work'
+        spend = self.spending_by_cat1(date_from, date_to, ignore_cat=exclude)
+        spendamount = 0
+        for row in spend:
+            spendamount += row['amount']
+
+        return [{'cat1':'spend','amount':spendamount}]
